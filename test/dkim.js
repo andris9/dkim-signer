1061378e0f833784435f2b86ac245bdc88091be2
Dave Gray <git@doesntsuck.com>
2016-08-22 18:11:29 -0600

    fix/enable required header test

--- test/dkim.js
+++ test/dkim.js
@@ -247,17 +247,15 @@ exports["Sign+verify tests"] = {
     },
     // Section 5.4: The From header field MUST be signed (that is, included in the "h="
     // tag of the resulting DKIM-Signature header field)
-/*
     "Sig format: h= missing From": function(test) {
         var mail = testMsg();
-        var dkimField = signMsg(mail, "node.ee", "dkim").replace('h=', '').replace('from:', '');
+        var dkimField = signMsg(mail, "node.ee", "dkim").replace('from:', '');
         dkim.DKIMVerify(dkimField + "\r\n" + mail, function(err, result) {
             test.equal(err, null);
-            test.deepEqual(result, {result: false, issue_desc: 'DKIM-Signature missing required tag bh'});
+            test.deepEqual(result, {result: false, issue_desc: 'DKIM-Signature h tag doesn\'t contain From'});
             test.done();
         });
     },
-*/
 
 /*
     // Additional RFC compliance tests
